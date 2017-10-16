78f4d5c8d5ba0d178c0f64aaf318b733fb62fb2e
DevopsPraj <30688469+DevopsPraj@users.noreply.github.com>
2017-10-16 13:21:17 +0530

    create-vm.sh

---
+++ create-vm.sh
@@ -0,0 +1,1 @@
+aws ec2 run-instances --image-id ${IMAGE_ID} --count ${count} --instance-type ${INSTANCE_TYPE} --key-name my-key --security-group-ids sg-daa564a1 --subnet-id subnet-33264e54 --associate-public-ip-address --query 'Instances[0].InstanceId' --output text
