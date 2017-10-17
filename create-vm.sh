5b833e74a42c7f6e2ebf8dc21afe6530e359ec29
DevopsPraj <30688469+DevopsPraj@users.noreply.github.com>
2017-10-17 14:05:20 +0530

    Update create-vm.sh

--- create-vm.sh
+++ create-vm.sh
@@ -1,3 +1,3 @@
-Instance_id=`aws ec2 run-instances --image-id ${AMI_ID} --count ${Num_of_Instances} --instance-type ${INSTANCE-TYPE} --key-name ${KEYNAME} --security-group-ids ${SECURITY_ID} --subnet-id {SUBNET_ID} --associate-public-ip-address --query 'Instances[0].InstanceId' --output text`
+Instance_id=`aws ec2 run-instances --image-id ${IMAGE_ID} --count ${Num_of_Instances} --instance-type ${INSTANCE-TYPE} --key-name ${KEYNAME} --security-group-ids ${SECURITY_ID} --subnet-id {SUBNET_ID} --associate-public-ip-address --query 'Instances[0].InstanceId' --output text`
 
 aws ec2 create-tags --resources $Instance_id --tags Key=Name,Value=${INSTANCE_NAME}
