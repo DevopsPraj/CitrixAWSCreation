257000a0585f59449f1a6f8756fc04d38aa194ee
DevopsPraj <30688469+DevopsPraj@users.noreply.github.com>
2017-10-17 15:23:36 +0530

    Update create-vm.sh

--- create-vm.sh
+++ create-vm.sh
@@ -2,6 +2,6 @@ aws configure set aws_access_key_id ${ACCESS_KEY}
 aws configure set aws_secret_access_key ${SECRET_KEY}
 aws configure set default.region ${REGION}
 hostname
-Instance_id=`aws ec2 run-instances --image-id ${IMAGE_ID} --count ${Num_of_Instances} --instance-type ${INSTANCE-TYPE} --key-name ${KEYNAME} --security-group-ids ${SECURITY_ID} --subnet-id {SUBNET_ID} --associate-public-ip-address --query 'Instances[0].InstanceId' --region=${REGION} --output text`
+Instance_id=`aws ec2 run-instances --image-id ${IMAGE_ID} --count ${Num_of_Instances} --instance-type ${INSTANCE-TYPE} --key-name ${KEYNAME} --security-group-ids ${SECURITY_ID} --subnet-id ${SUBNET_ID} --associate-public-ip-address --query 'Instances[0].InstanceId' --region=${REGION} --output text`
 
 aws ec2 create-tags --resources $Instance_id --tags Key=Name,Value=${INSTANCE_NAME}
